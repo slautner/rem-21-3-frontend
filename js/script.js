2a0ed7ca2e6e158d34cdf822a123b37e9b205cc9
Stefan Lautner <stefan.lautner@highpace.de>
2021-08-03 15:56:42 +0200

    add math functions

---
+++ js/script.js
@@ -0,0 +1,36 @@
+function add(first, second) {
+    return first + second
+}
+
+const functionAdd = add(3, 2);
+console.log(`Functional: ${functionAdd}`);
+
+// ---
+
+const arrowAddFull = (first, second) => {
+    return first + second
+}
+console.log(`Full: ${arrowAddFull(5, 5)}`);
+
+const arrowAddShort = (first, second) => first + second
+console.log(`Short: ${arrowAddShort(3, 5)}`);
+
+// ---
+
+const factorialWhile = (factor) => {
+    let i = 1;
+    let result = 1;
+    while (i < factor) {
+        i++;
+        result *= i;
+    }
+    return result;
+}
+console.log(`Factorial of 0 = ${factorialWhile(0)}`);
+console.log(`Factorial of 3 = ${factorialWhile(3)}`);
+console.log(`Factorial of 4 = ${factorialWhile(4)}`);
+
+const factorial = n => (n === 0 ? 1 : n * factorial(n -1))
+console.log(`Factorial of 0 = ${factorial(0)}`);
+console.log(`Factorial of 3 = ${factorial(3)}`);
+console.log(`Factorial of 4 = ${factorial(4)}`);
