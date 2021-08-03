700a76ff1a851968f0de55d13908cc896cce5cce
Stefan Lautner <stefan.lautner@highpace.de>
2021-08-03 17:07:09 +0200

    add mutable, immutable array rename

--- js/script.js
+++ js/script.js
@@ -27,3 +27,42 @@ const factorial = n => (n === 0 ? 1 : n * factorial(n - 1))
 console.log(`Factorial of 0 = ${factorial(0)}`);
 console.log(`Factorial of 3 = ${factorial(3)}`);
 console.log(`Factorial of 4 = ${factorial(4)}`);
+
+// ---
+
+const studentKlaus = {
+    id: "1",
+    name: "Klaus"
+}
+const studentMarie = {
+    id: "2",
+    name: "Marie"
+}
+
+const students = [studentKlaus, studentMarie]
+console.log(`Origin array: ${JSON.stringify(students)}`)
+
+function renameImmutable(students, name, newName) {
+    const studentsCopy = [...students]
+    const studentFoundIndex = studentsCopy.findIndex(student => student.name === name)
+    if (studentFoundIndex >= 0) {
+        const studentCopy = {...studentsCopy[studentFoundIndex]}
+        studentCopy.name = newName
+        studentsCopy[studentFoundIndex] = studentCopy;
+    }
+    return studentsCopy;
+}
+
+console.log(`Immutable changed: ${JSON.stringify(renameImmutable(students, "Klaus", "Bernd"))}`)
+console.log(`Origin array: ${JSON.stringify(students)}`)
+
+function renameMutable(students, name, newName) {
+    const studentFound = students.find(student => student.name === name)
+    if (studentFound) {
+        studentFound.name = newName;
+    }
+    return students;
+}
+
+console.log(`Mutable changed: ${JSON.stringify(renameMutable(students, "Klaus", "Bernd"))}`)
+console.log(`Modified (origin) array: ${JSON.stringify(students)}`)
